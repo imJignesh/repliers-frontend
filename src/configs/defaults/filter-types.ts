77e675810c89476fd11297b08b490210ca9930dc
AnkurSMB <imjignesh2@gmail.com>
2025-11-05 14:25:50 +0530

    initial

---
+++ src/configs/defaults/filter-types.ts
@@ -0,0 +1,40 @@
+// TODO: rename SCREAMING_SNAKE_CASE
+export const TYPE_RESIDENTIAL = [
+  'Detached',
+  'Cottage',
+  'Rural Resid',
+  'Modular Home'
+]
+export const TYPE_CONDO = [
+  'Condo Apt',
+  'Comm Element Condo',
+  'Det Condo',
+  'Condo Townhouse',
+  'Semi-Det Condo',
+  'Co-Op Apt',
+  'Leasehold Condo',
+  'Co-Ownership Apt'
+]
+export const TYPE_TOWNHOME = ['Att/Row/Twnhouse', 'Link']
+export const TYPE_SEMIDETACHED = ['Semi-Detached']
+export const TYPE_MULTI_FAMILY = ['Multiplex', 'Duplex', 'Triplex', 'Fourplex']
+export const TYPE_LAND = ['Land', 'Vacant Land', 'Vacant Land Condo']
+export const TYPE_COMMERCIAL = [
+  'Commercial/Retail',
+  'Office',
+  'Farm',
+  'Store W/Apt/Office',
+  'Investment',
+  'Industrial'
+]
+export const TYPE_BUSINESS = ['Sale Of Business']
+export const TYPE_RENTAL = ['Residential Rental']
+
+export const STYLE_RESIDENTIAL = []
+export const STYLE_TOWNHOME = []
+export const STYLE_CONDO = []
+export const STYLE_SEMIDETACHED = []
+export const STYLE_MULTI_FAMILY = []
+export const STYLE_LAND = []
+export const STYLE_COMMERCIAL = []
+export const STYLE_BUSINESS = []
